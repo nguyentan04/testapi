6eb50214ee46559dc8abfe59c03bdc0fad812f58
themagicalfinance <100901957+themagicalfinance@users.noreply.github.com>
2022-06-07 22:49:41 +0700

    update

--- js/min.js
+++ js/min.js
@@ -58,3 +58,20 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+var swiper = new Swiper( '.swiper-container.two', {
+  pagination: '.swiper-pagination',
+  paginationClickable: true,
+  effect: 'coverflow',
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflow: {
+    rotate: 0,
+    stretch: 100,
+    depth: 150,
+    modifier: 1.5,
+    slideShadows : false,
+  }
+} );
